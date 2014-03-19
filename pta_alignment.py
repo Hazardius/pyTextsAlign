531937b3343cf586b06e7fdda700e35903341dfc
Hazardius <hazardiusam@gmail.com>
2014-03-19 21:42:34 +0100

    Creation of repo.

---
+++ pta_alignment.py
@@ -0,0 +1,5 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This file contains alignement methods used by a pyTextsAlign.
+"""
---
+++ pta_files.py
@@ -0,0 +1,5 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This file contains logic of file IN/OUT operations.
+"""
